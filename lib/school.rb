81f02f637bf16b8967f7845967fd6f05896736c4
Gevy Cunanan <gevycunanan@icloud.com>
2020-11-28 12:46:53 -0500

    commit tree 3b910ae367e7df1833f8ebb74bec9435bb934817

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School 
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster.detect do |x, y|
+            if x == grade
+                return y
+            end
+        end
+    end
+
+    def sort
+        new_sort = {}
+        roster.each do |x, y|
+            new_sort [x] = y.sort 
+        end
+        new_sort
+    end
+end
+
+
+
